c94f2137d850a607cf6b4e5aec9ef6fb70f01170
Kevin App <kevinapp34@googlemail.com>
2021-06-15 15:51:50 +0200

    changed cInterface.py

--- cInterface.py
+++ cInterface.py
@@ -1,10 +1,8 @@
 import tkinter
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-
 import numpy as np
 
 
